7ab2d1df22be3d0d67d5efa23900b8f42dd9f695
ENGI_Tsuyuzaki <tsuyuzaki.tatsuya@engi-st.jp>
2021-08-03 19:09:13 +0900

    menuを読み込む用に変更

--- executionScript.py
+++ executionScript.py
@@ -1,9 +1,9 @@
 ''' LibraryReload '''
 #from MayaLibrary import setCurves as ml; reload(ml);
 ''' Run '''
-#from mainEdit import skinPaintValue as ps
-from MayaExecute import setCurves_antenna as ps
+#from scriptsInTools.singleRun import skinPaintValue as ps
+from mgear_guide._tools.Manager.Maya import mgearDict as ps
 reload(ps); ps.main()
 
 #import pymel.core as pm
-#import maya.cmds as cmds
+#import maya.cmds as cmd
