ba23e575b53e4d3d649824ff81b4291c6f51a325
ENGI_Tsuyuzaki <tsuyuzaki.tatsuya@engi-st.jp>
2021-08-16 19:07:09 +0900

    読み込みフォルダーの階層を変更したことによりfromを変更

--- executionScript.py
+++ executionScript.py
@@ -2,7 +2,7 @@
 #from MayaLibrary import setCurves as ml; reload(ml);
 ''' Run '''
 #from scriptsInTools.singleRun import skinPaintValue as ps
-from mgear_guide._tools.Manager.Maya import mgearDict as ps
+from scriptsInTools.Maya.Manager import selectBind as ps
 reload(ps); ps.main()
 
 #import pymel.core as pm
