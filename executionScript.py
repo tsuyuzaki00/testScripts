c2e991496b2c5428a281b64106361f508b5d6d80
ENGI_Tsuyuzaki <tsuyuzaki.tatsuya@engi-st.jp>
2021-09-09 19:34:50 +0900

    更新更新

--- executionScript.py
+++ executionScript.py
@@ -2,7 +2,8 @@
 #from mgear_guide._tools.UI import mgearVisExportUI as UI; reload(UI);
 ''' Run '''
 #from mgear_guide._tools.Maya.Execute import mgearExportCurves as ps
-from scriptsInTools.Maya.Execute import multiCopySkinWeight as ps
+#from scriptsInTools.Maya.Execute import multiCopySkinWeight as ps
+from scriptsInTools.Maya.Manager import selectListView as ps
 reload(ps); ps.main()
 
 #import pymel.core as pm
